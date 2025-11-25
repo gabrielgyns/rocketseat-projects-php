72870cb706dce373975084c4edcdaebf8908fc03
Gabriel Soares <gabrielsoares@ba:14:15:74:4f:36.lan>
2025-11-25 15:26:42 +0000

    feat: updating portfolio 1

--- 1-portfolio/components/hero.php
+++ 1-portfolio/components/hero.php
@@ -1,24 +1,24 @@
 <?php
-    $itens = [
-        ['href' => '#', 'src' => '/img/twitter.png', 'alt' => 'Twitter Logo'],
-        ['href' => '#', 'src' => '/img/facebook.png', 'alt' => 'Facebook Logo'],
-        ['href' => '#', 'src' => '/img/linkedin.png', 'alt' => 'LinkedIn Logo'],
-        ['href' => '#', 'src' => '/img/youtube.png', 'alt' => 'YouTube Logo']
-    ];
+$itens = [
+    ['href' => '#', 'src' => '/img/twitter.png', 'alt' => 'Twitter Logo'],
+    ['href' => '#', 'src' => '/img/facebook.png', 'alt' => 'Facebook Logo'],
+    ['href' => '#', 'src' => '/img/linkedin.png', 'alt' => 'LinkedIn Logo'],
+    ['href' => '#', 'src' => '/img/youtube.png', 'alt' => 'YouTube Logo']
+];
 ?>
 
 <section class="flex gap-x-3">
     <!-- Título e Descrição -->
     <div class="w-2/3">
-        <h1 class="text-3xl font-bold">Oi, meu nome é Rafael.</h1>
+        <h1 class="text-3xl font-bold">Oi, meu nome é Gabriel.</h1>
 
         <p class="text-xl leading-6 mt-6">
             Falando um pouco sobre mim, sou um desenvolvedor web que adora criar coisas novas e aprender novas tecnologias.
-            Especializado em PHP e HTML, mas também tenho conhecimento em outras linguagens como Python e Java.
+            Especializado em Front com React, mas também tenho alguns conhecimentos no backend também.
         </p>
 
         <ul class="flex gap-x-3 mt-3">
-            <?php foreach($itens as $item): ?>
+            <?php foreach ($itens as $item): ?>
                 <li>
                     <a href="<?= $item['href'] ?>" target="_blank">
                         <img class="h-8 hover:animate-bounce" src="<?= $item['src'] ?>" alt="<?= $item['alt'] ?>">
--- 1-portfolio/components/projects.php
+++ 1-portfolio/components/projects.php
@@ -1,41 +1,41 @@
 <?php
-    $projetos = [
-        [
-            "titulo" => "Meu Portfolio",
-            "finalizado" => false,
-            "ano" => 2021,
-            "descricao" => "Meu primeiro Portfolio. Escrito em PHP e HTML.",
-            "stack" => ["PHP", "HTML", "CSS", "JS"],
-            "img" => "/img/projeto1.png"
-        ],
-        [
-            "titulo" => "Lista de Tarefas",
-            "finalizado" => true,
-            "ano" => 2022,
-            "descricao" => "Lista de Tarefas. Escrito em PHP e HTML.",
-            "stack" => ["PHP", "HTML", "CSS", "JS"],
-            "img" => ""
-        ],
-        [
-            "titulo" => "Controle de Leitura de Vídeos",
-            "finalizado" => true,
-            "ano" => 2024,
-            "descricao" => "Lista de Livros. Escrito em PHP e HTML.",
-            "stack" => ["PHP", "HTML", "CSS", "JS"],
-            "img" => ""
-        ],
-        [
-            "titulo" => "Mais um Projeto",
-            "finalizado" => false,
-            "ano" => 2025,
-            "descricao" => "Projeto secreto em andamento. Escrito em PHP e HTML.",
-            "stack" => ["PHP", "HTML", "CSS", "JS"],
-            "img" => ""
-        ],
-    ];
+$projetos = [
+    [
+        "titulo" => "Meu Portfolio",
+        "finalizado" => false,
+        "ano" => 2021,
+        "descricao" => "Meu primeiro Portfolio. Escrito em PHP e HTML.",
+        "stack" => ["PHP", "HTML", "CSS", "JS"],
+        "img" => "/img/projeto1.png"
+    ],
+    [
+        "titulo" => "Lista de Tarefas",
+        "finalizado" => true,
+        "ano" => 2022,
+        "descricao" => "Lista de Tarefas. Escrito em PHP e HTML.",
+        "stack" => ["PHP", "HTML", "CSS", "JS"],
+        "img" => "/img/projeto1.png"
+    ],
+    [
+        "titulo" => "Controle de Leitura de Vídeos",
+        "finalizado" => true,
+        "ano" => 2024,
+        "descricao" => "Lista de Livros. Escrito em PHP e HTML.",
+        "stack" => ["PHP", "HTML", "CSS", "JS"],
+        "img" => "/img/projeto1.png"
+    ],
+    [
+        "titulo" => "Mais um Projeto",
+        "finalizado" => false,
+        "ano" => 2025,
+        "descricao" => "Projeto secreto em andamento. Escrito em PHP e HTML.",
+        "stack" => ["PHP", "HTML", "CSS", "JS"],
+        "img" => "/img/projeto1.png"
+    ],
+];
 ?>
 
-<?php foreach($projetos as $projeto): ?>
+<?php foreach ($projetos as $projeto): ?>
     <div class="bg-slate-800 rounded-lg p-3 flex items-center space-x-3">
         <div class="w-1/5 flex items-center justify-middle">
             <img src="<?= $projeto['img'] ?>" class="w-42 rounded-md">
@@ -49,21 +49,22 @@
                     <?php endif; ?>
                     <?= $projeto['titulo'] ?>
                     <?php if ($projeto['finalizado']): ?>
-                        <span class="text-xs text-grey-400 opacity-50 italic">( Finalizado em <?= $projeto['ano'] ?> )</span></h3>
-                    <?php else: ?>
-                        <span class="text-xs text-grey-400 opacity-50 italic">( Em desenvolvimento )</span></h3>
-                    <?php endif; ?>
+                        <span class="text-xs text-grey-400 opacity-50 italic">( Finalizado em <?= $projeto['ano'] ?> )</span>
+                </h3>
+            <?php else: ?>
+                <span class="text-xs text-grey-400 opacity-50 italic">( Em desenvolvimento )</span></h3>
+            <?php endif; ?>
 
-                <div class="space-x-1">
-                    <?php 
-                        $colors = ['fuchsia', 'lime', 'sky', 'rose', 'amber', 'teal', 'purple'];
-                        foreach($projeto['stack'] as $posicao => $language):
-                    ?>
-                        <span class="bg-<?= $colors[$posicao] ?>-400 text-<?= $colors[$posicao] ?>-900 rounded-md px-2 py-1 font-semibold text-xs">
-                            <?= $language ?>
-                        </span>
-                    <?php endforeach; ?>
-                </div>
+            <div class="space-x-1">
+                <?php
+                $colors = ['fuchsia', 'lime', 'sky', 'rose', 'amber', 'teal', 'purple'];
+                foreach ($projeto['stack'] as $posicao => $language):
+                ?>
+                    <span class="bg-<?= $colors[$posicao] ?>-400 text-<?= $colors[$posicao] ?>-900 rounded-md px-2 py-1 font-semibold text-xs">
+                        <?= $language ?>
+                    </span>
+                <?php endforeach; ?>
+            </div>
             </div>
 
             <p class="leading-6"><?= $projeto['descricao'] ?></p>
